e30b7bf7db5bc22c6e655bf68109f55029ad67f7
Josue Ortiz <20220329@ricaldone.edu.sv>
2024-07-12 13:27:18 -0600

    Proyecto con Registro y Login terminado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Practica-firebase-20220329" 
+--Firebase autheticate
+npm install @react-native-firebase/app @react-native-firebase/auth
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +21,10 @@ console.log("Valor de la configuracion", firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Obtén referencias a los servicios que necesitas
+const auth = getAuth(app);
+
 const database = getFirestore(app);
 const storage = getStorage(app);
-export { database, storage };
+
+export { auth, database, storage };
